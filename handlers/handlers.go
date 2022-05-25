a471a252c0d04769d4febf8a3e96bd968917e8bd
petrostrak <pit.trak@gmail.com>
2022-05-25 20:53:05 +0300

    Add ListFS handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,9 +2,14 @@ package handlers
 
 import (
 	"net/http"
+	"net/url"
 
-	"github.com/petrostrak/sokudo"
 	"myapp/data"
+
+	"github.com/CloudyKit/jet/v6"
+	"github.com/petrostrak/sokudo"
+	"github.com/petrostrak/sokudo/filesystems"
+	"github.com/petrostrak/sokudo/filesystems/miniofilesystem"
 )
 
 // Handlers is the type for handlers, and gives access to Celeritas and models
@@ -18,5 +23,50 @@ func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 	err := h.render(w, r, "home", nil, nil)
 	if err != nil {
 		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
+
+func (h *Handlers) ListFS(w http.ResponseWriter, r *http.Request) {
+	var (
+		fs   filesystems.FS
+		list []filesystems.Listing
+	)
+
+	fsType := ""
+	if r.URL.Query().Get("fs-type") != "" {
+		fsType = r.URL.Query().Get("fs-type")
+	}
+
+	curPath := "/"
+	if r.URL.Query().Get("curPath") != "" {
+		curPath = r.URL.Query().Get("curPath")
+		curPath, _ = url.QueryUnescape(curPath)
+	}
+
+	if fsType != "" {
+		switch fsType {
+		case "MINIO":
+			f := h.App.FileSystems["MINIO"].(miniofilesystem.Minio)
+			fs = &f
+			fsType = "MINIO"
+		}
+
+		l, err := fs.List(curPath)
+		if err != nil {
+			h.App.ErrorLog.Println(err)
+			return
+		}
+
+		list = l
+	}
+
+	vars := make(jet.VarMap)
+	vars.Set("list", list)
+	vars.Set("fs_type", fsType)
+	vars.Set("curPath", curPath)
+
+	err := h.render(w, r, "list-fs", vars, nil)
+	if err != nil {
+		h.App.ErrorLog.Println(err)
 	}
 }
