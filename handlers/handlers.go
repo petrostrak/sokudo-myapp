3bc48bd2a6c7df2bd6862cad893779270deac0ed
petrostrak <pit.trak@gmail.com>
2022-05-25 21:08:02 +0300

    Add getFileToUpload func

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"os"
 
 	"myapp/data"
 
@@ -81,4 +84,34 @@ func (h *Handlers) UploadToFS(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		h.App.ErrorLog.Println(err)
 	}
+}
+
+func (h *Handlers) PostUploadToFS(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getFileToUpload(r *http.Request, fieldName string) (string, error) {
+	err := r.ParseMultipartForm(10 << 22)
+	if err != nil {
+		return "", err
+	}
+
+	file, header, err := r.FormFile(fieldName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	dst, err := os.Create(fmt.Sprintf("./tmp/%s", header.Filename))
+	if err != nil {
+		return "", err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("./tmp/%s", header.Filename), nil
 }
