2f071f9a60b9b2003eedcbde6c4addf8247c468a
petrostrak <pit.trak@gmail.com>
2022-05-28 10:18:09 +0300

    Separate Web and API routes

--- routes.go
+++ routes.go
@@ -33,5 +33,7 @@ func (a *application) routes() *chi.Mux {
 	fileServer := http.FileServer(http.Dir("./public"))
 	a.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
 
+	a.App.Routes.Mount("/api", a.ApiRoutes())
+
 	return a.App.Routes
 }
